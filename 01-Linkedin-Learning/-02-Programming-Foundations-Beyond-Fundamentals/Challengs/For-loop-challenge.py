c5004f7bac03cc8ea1f0e745f3343aa67fbc842b
Amr <amrya200t@gmail.com>
2022-07-02 20:27:32 +0200

    Update For-loop-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
@@ -1,1 +1,13 @@
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
 
+print("Our fruits selection:")
+
+for fruit in fruits:
+    print(fruit)
