953b572d2de25fd9db9d8a607cd1ab36604b3656
Amr <amrya200t@gmail.com>
2022-07-13 12:38:07 +0200

    Create Challenge 2.js

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 1/02-Challenges/Challenge 2.js
@@ -0,0 +1,19 @@
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+const isMarkHigherBMI = markMass / markHeight ** 2 > johnMass / johnHeight ** 2;
+
+if (isMarkHigherBMI) {
+  console.log(
+    `Mark's BMI (${(markMass / markHeight ** 2).toFixed(
+      1
+    )}) is higher than John's (${(johnMass / johnHeight ** 2).toFixed(1)})!`
+  );
+} else {
+  console.log(
+    `John's BMI (${(johnMass / johnHeight ** 2).toFixed(
+      1
+    )}) is higher than Mark's (${(markMass / markHeight ** 2).toFixed(1)})!`
+  );
+}
