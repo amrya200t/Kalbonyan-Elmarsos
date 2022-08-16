c31b143174c8d2a778e6eef7d5151decf9c311b1
Amro Ahmed <amrya200t@gmail.com>
2022-08-16 04:55:20 +0200

    Redux Cart(S19)

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/redux-cart/src/store/cart-actions.js
@@ -0,0 +1,83 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://react-http-6e36b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+      if (!res.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchRequest = async () => {
+      const res = await fetch(
+        "https://react-http-6e36b-default-rtdb.europe-west1.firebasedatabase.app/cart.json"
+      );
+      if (!res.ok) {
+        throw new Error("Fetching cart data failed.");
+      }
+
+      return await res.json();
+    };
+
+    try {
+      const cartData = await fetchRequest();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching cart data failed!",
+        })
+      );
+    }
+  };
+};
